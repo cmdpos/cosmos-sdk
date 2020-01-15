5f85612cd347e37572f70b0e107fc43b9a3fb1a1
zhongqiuwood <zhongqiuwood@gmail.com>
2020-01-15 19:19:49 +0800

    more query

--- dev/query.sh
+++ dev/query.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 
+gaiacli query distr commission  evavaloper1pjx74f0l6nvwx857e8m5x78fepph4rresakmn3 --node http://18.163.89.47:20181
+
+########
+
 gaiacli query account cosmos1hg40dv5e237qy28vtyum52ygke32ez35hm307h --chain-id testchain --node localhost:26657
 gaiacli query account cosmos1geyy4wtak2q9effnfhze9u4htd8yxxmagdw3q0 --chain-id testchain --node localhost:26657
 
@@ -25,6 +29,8 @@ gaiacli query distr commission  evavaloper1pjx74f0l6nvwx857e8m5x78fepph4rresakmn
 gaiacli query distr community-pool   --node http://18.163.89.47:20181
 
 gaiacli query staking delegations-to evavaloper1pjx74f0l6nvwx857e8m5x78fepph4rresakmn3 --node http://18.163.89.47:20181
+gaiacli query staking delegations-to evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181
+
 
 community-pool
 
@@ -54,6 +60,10 @@ gaiacli query staking delegation eva1qpfqusq9atcmag6nmjhh8age3jaznw0nwrjg5j evav
 gaiacli query distr rewards eva1qpfqusq9atcmag6nmjhh8age3jaznw0nwrjg5j evavaloper132q0hvhfjx84wl04ez9urnvqs3f7futqr6la5t --node http://18.163.89.47:20181
 
 
+gaiacli query staking delegation eva1hryyxy3m6hxwmdqzg7r260lm704ua8engerd9h evavaloper132q0hvhfjx84wl04ez9urnvqs3f7futqr6la5t --node http://18.163.89.47:20181
+
+
+gaiacli query account eva1hryyxy3m6hxwmdqzg7r260lm704ua8engerd9h --node http://18.163.89.47:20181
 
 
 
@@ -62,4 +72,25 @@ gaiacli query distr rewards eva1qpfqusq9atcmag6nmjhh8age3jaznw0nwrjg5j evavalope
 
 
 
-
+gaiacli query distr rewards eva1qnkgg9h04v4avc79lzqj9tgdztzlw4e8454mvm  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva1pjx74f0l6nvwx857e8m5x78fepph4rrexq5dh5  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva19qv9zx0a0p2l02kcfrndv6k99wknadu04twzmv  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva19gcs0mp6fsusnmuxzsjrza97srfzv3mu5kdn5x  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva18h38cm2eap4k5jxetwwk4yq5zjze3wfsmlxpc4  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva1fsvvrkwvlh7084mwlpjek4vjm04enljejpl7z4  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva1dukare522acjd0up8ll3tpeh7rdt35q7jlu2r6  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva10kclpxhy9mxavdvyg3k20x9jfqqn7rlkyxz500  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva10hs433nww8pv5ru0r4x3xrm64xgwqgavdf6yxe  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva136ytju4l3twf2jjxgd7etr8527m5q9acp89glw  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva1jqr46kdxsnk7h8tnkh96e5uglvalxxawye5k8j  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva1j969dh9klyx43ej8s5rrd0j7xa95ym7ncxepa0  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva14pevscmc7lvnkmkfutp09wcn0ecgaldxsna03m  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva1ht5rs8d3sgy9882yrvcd79phskc70q2affm8nj  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva1he7hvqaycmgw0h3x56tku8jye5tj683z88tyax  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva1c8sfzqhtatquq07fxn2fj4vt6qzarmnkg4xr9h  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva1c3yhug3ztfkujq6unktdk9h9nmduk7fzackq00  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva16ttau6apv0fvh0m8e0tk2drkdvfaxlapt4mvdh  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva167qjyr23fcm50ptwpupa67v8ef5jk7keuap5fu  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva1uyvqg0z59st95x9hy4tde037r2x6tqev4f2w5k  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva1uxy3cldwdyxhzmauh4tdjg0uyfdrqcmr0mdf9t  --node http://18.163.89.47:20181
+gaiacli query distr rewards eva17w56q0yzuvf42z9k0zrlrvlezect2jvxuauset  --node http://18.163.89.47:20181
---
+++ dev/query3.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+date
+gaiacli query distr rewards eva1zz50hsy2t6d689k4s6qqdxh6nahpqhjvem4yxp  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1yp6al6p76trt28md3w2xesaupfnmcrrx5l50s6  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva19sf4epggywej4gs2u8ad3m0p9e7yhrgty42wza  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1x970apxvelzmceky95na6xwagvrh6kfz95t0x8  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva18r0va46sskg8ywq2f2s0g2fz6ssyc9zmnlldcm  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva18gx7ckl03rvw4mrl639am4k0tcsksmg2uasj02  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1gzj8nerczy92h3nn6lzg6g22vytqdmc2mp6xuh  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1gyqmmq97c28cxyvmh0uhvxrw06d9hq0l8h20sv  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1ft40keayad5ffweajdxssstrsg90pvxw7pvuky  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1ttnyvj87tv4lugs0dyuh35uny9rulfms8jejdk  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1tkmaag77yd33dzwypna7qstkgx8wne7ngx7mt8  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1tmgdpczz5z7y3wjps5xvlds74atecn4ttppkdt  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1v9jksaw4dz6dya6gtjhpplrcxfaye5vl4n79x5  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1v8vsp80wz02zhqsg8rlqj5hdcugyjhzkxeex9a  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1va7hatgd75pzaf0p7knp5gxx5ucet34nj9tpxs  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1wt4g5jj22z5fka5jty8t7a4arjf4mn26udx6nq  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1wanhdz3ygelva4q7d93x2pf4nsen67gju4rjuu  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1sf2s9updshdlnkzm0am9hwrdgf2lfxpth4n62t  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1sn480pea777ktf8fmqazqqawx9sraz073e7wue  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1j969dh9klyx43ej8s5rrd0j7xa95ym7ncxepa0  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1nc9rwt4l854mk83gy7n7cxalkva6cqfvws7cu6  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva14qgtwrees3vpuphzj2323navd3qn0mp2yv6e7n  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1kxuwvj6zt8yhx9d6w5l5mfc0s7gckc69sjrz6c  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1h7jpq22777tpx86eqm2kpg3qk95pt93zhuazk4  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1chlf962hqnnknnkrv0hur2x2x5us0m90dmejm2  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1ekahfrkqens8zq6y736a9t6yyl3xgul9ecpwmw  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1e7tfnrdzr5wd9wlvmqf5eltxj66dcw2ygdfede  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva16qqjjs8gzehgwqweqv9l825rwensvgnkuvrkqh  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva16ls59sm5hc63h5szj3z7wv27xvw5xfn3s3wmzm  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1mpgqadadly0qndue49fkld2vptf4jpa4824pzw  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1mke2ylp6p6am5gvrsdhc3w8yflmyf6grzy4z0k  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1u2s3fxlqtst7e3n2z2yt3kqk9wuhmhct73dzwl  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1uvyma94qyvsx9w3tzrgelg5kqmxcmkxj4pk56z  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1u5gapxpeqxr8etshvfx5w76qu8axkepvdrg9hk  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1a9u9us0n5gyqku0azsmsuz69vnqyq9veyx9l0w  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva17y267fkaezzz5v2tm0gxzdm02rw7l0dp3zzkd3  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva173d8z3qdf00r59909m8kezrsuuhsff88fu3c3r  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+gaiacli query distr rewards eva1lpmu4rdnyugna059exj5p5mmj4rrwksq3um0ja  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
+
+gaiacli query distr commission  evavaloper16ls59sm5hc63h5szj3z7wv27xvw5xfn3xvvdx7 --node http://18.163.89.47:20181 -o text
