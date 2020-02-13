fea86dc8c7b2484221d58667b395962b14e81f2c
qzhongwood <2609611@qq.com>
2020-02-13 10:03:40 +0800

    query all nodes

--- dev/testnet/qaccount.sh
+++ dev/testnet/qaccount.sh
@@ -7,16 +7,25 @@ COSMOS_TOP=${GOPATH}/src/github.com/cmdpos/cmapp
 
 CHAIN_NODE="--node tcp://localhost:10057"
 
-query_account() {
+
+
+loop_nodes() {
     for (( i=0; i<${1}; i++)) do
         if [ -d cache/node${i} ]; then
             echo "============================================================="
             echo "node${i} account details:"
             CHAIN_HOME="--home ${CUR_DIR}/cache/node${i}/gaiacli"
             CHAIN_ENV0="--node tcp://localhost:10057 --home "${CUR_DIR}"/cache/node${i}/gaiacli --chain-id testchain"
-        gaiacli query account $(gaiacli keys show node${i} -a ${CHAIN_HOME}) ${CHAIN_ENV0}
+
+            delegater_address=$(gaiacli keys show node${i} -a ${CHAIN_HOME})
+            operator_address=$(gaiacli keys show node${i} --bech val -a ${CHAIN_HOME})
+
+            gaiacli query account $delegater_address ${CHAIN_ENV0}
+            gaiacli query distr rewards $delegater_address ${CHAIN_ENV0}
+            gaiacli query staking validator $operator_address ${CHAIN_ENV0}
         fi
     done
 }
 
-query_account 10
+
+loop_nodes 10
